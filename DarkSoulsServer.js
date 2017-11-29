258c00f4d1d54ea033c112fb9b4bb13c9e7ca301
Christian MacDonald <cmacdona@nmu.edu>
2017-11-28 21:49:48 -0500

    the basic server... not making commands to the data base yet.

--- DarkSoulsServer.js
+++ DarkSoulsServer.js
@@ -10,16 +10,33 @@ var bodyParser = require("body-parser");
 server.use(bodyParser.urlencoded({extended: false}));
 server.use(express.static("pub")); //static files served up (like index.html)
 
+var paramTypes = ["price", "name", "location","type"];
 
-server.use("/postGetData", function(req, res) { //"/test" is the URL we are associating this with.  req is the "request object" and res is the "response object".  These encapsulate the request from the HTTP client and the response that we are giving back to them.
-	res.write("Hello " + req.body.person + " from a POST");
+server.use("/list", function(req, res) { //"/test" is the URL we are associating this with.  req is the "request object" and res is the "response object".  These encapsulate the request from the HTTP client and the response that we are giving back to them.
+
+    /*
+    var param;
+    var val;
+
+    if (!(req.includes(':'))) { res.write("Invalid Entry... No ':' included."); res.end();}
+
+    var myString = req.split(':');
+    param = myString[0];
+    val = myString[1];
+
+    if (param == "" || val == "") { res.write("Invalid Entry... Form is parameter:value"); res.end(); }
+    param = param.toLowerCase();
+    val = val.toLowerCase();
+
+    if (!(paramTypes.includes(param))) { res.write("Invalid Parameter Entry... Parameter Types include 'price' 'name' 'location' or 'type'."); res.end(); }
+    */
+
+    res.write("Hello " + req.body.person + " from a POST");
 	res.end(); //ends the response.
 });
 
-server.use("/postUpdateData", function(req, res) { //"/test" is the URL we are associating this with.  req is the "request object" and res is the "response object".  These encapsulate the request from the HTTP client and the response that we are giving back to them.
-	res.write("Hello " + req.body.person + " from a POST");
-	res.end(); //ends the response.
-});
+
+
 
 server.listen(80, function() {
 	console.log("Server is running and ready for connections...");
