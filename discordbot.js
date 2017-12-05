bd4076a12c77f5e6d9714435f551b994e7cf6f1a
Jerry Ryan <jryan011724@gmail.com>
2017-12-04 21:18:00 -0500

    run discordbot.js on linux

--- discordbot.js
+++ discordbot.js
@@ -1,17 +1,39 @@
 //code is super barebones atm. Just placeholders until DB is ready
+//Try connection mongo to 35.24.106.170/DarkSoulsData. If we run this localhost just do localhost/DarkSoulsData.
 const Discord = require('discord.js');
 const Client = new Discord.Client();
+const mongoClient = require("mongodb").MongoClient;
+var express = require("express");
+var io = require('socket.io')(socketaddr);
+var socketaddr = "35.24.106.170:8080";
+var socket = io();
+io.on('connection',function(client){});
+
 Client.on("ready", ()=>{
 	console.log("ready!");
+});
+io.on("connection",function(socket){
+	
+	socket.on("searchReturn", function(data) {
+		active_queries.push(JSON.parse(data));
+		updateContent();
+	});
+	socket.on("listReturn", function(data) {
+		active_queries.push(JSON.parse(data));
+		updateContent();
+	});
 });
 
 Client.on("message", (message)=> {
     if (message.content.startsWith("!list")) {
-    message.channel.send("list working!");
+		var query = message.content.replace("!list ","");
+		var result = socket.emit("list", "Axes");
+		console.log(query);
+		message.channel.send();
   }
    if (message.content.startsWith("!search")) {
-    message.channel.send("search working!");
+		var query = message.replace("!search","");
+		message.channel.send(socket.emit("list","dagger"));
   }
 });
 Client.login("Mzc4NTk1Njc5NzUwMDYyMDgx.DP-dsA.5palAk_mPGrV32Tru1rwAaP12gw");
-
